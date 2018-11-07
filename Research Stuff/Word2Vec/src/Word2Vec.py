b7a12c1c37dd8542cb32079ef15292a86d1158d1
John Rey Juele <15167798@studentmail.ul.ie>
2018-11-07 10:03:50 +0000

    Adding more texts to dataset

--- Research Stuff/Word2Vec/src/Word2Vec.py
+++ Research Stuff/Word2Vec/src/Word2Vec.py
@@ -17,13 +17,16 @@ def get_wiki(title: str):
 
 if __name__ == "__main__":
 
-    file_handler = FileHandler()
+    file_handler = FileHandler("TextFiles")
+
     corpus = file_handler.get_sentence_processor_iterator()
 
-    model = Word2Vec(corpus, iter=50, min_count=10, size=300, workers=4)
+    model = Word2Vec(corpus, iter=50, min_count=15, size=300, workers=4, window=10)
 
-    model.save(path.join(file_handler.model_file_path, "computer_model"))
+    print(model.wv.most_similar(positive="intelligence"))
 
+    #print(model.wv.similarity(w1="security", w2="network"))
 
+    #print(model.wv["security"])
 
 
