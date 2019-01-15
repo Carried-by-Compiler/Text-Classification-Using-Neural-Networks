f4bf2870c82d091d6bb00f1b7f923264c81eb911
John Rey Juele <15167798@studentmail.ul.ie>
2019-01-15 11:24:39 +0000

    Working small prototype

--- Research Stuff/Doc2Vec/Main.py
+++ Research Stuff/Doc2Vec/Main.py
@@ -3,7 +3,9 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 from DataSetBuilder import DataSetBuilder
 from FileHandler import FileHandler
+from TopicClassifier import TopicClassifier
 import gensim
+import numpy as np
 
 
 def fill_data(name=""):
@@ -20,16 +22,66 @@ if __name__ == "__main__":
     """
     make_file("William Shakespeare", "Literature")
     """
+    docs = list()
+    # Find a better way of getting the labels for training!
+    labels = [[0, 0, 0, 1],
+              [0, 1, 0, 0],
+              [0, 0, 1, 0],  # 3
+              [1, 0, 0, 0],
+              [1, 0, 0, 0],
+              [1, 0, 0, 0],
+              [1, 0, 0, 0],
+              [1, 0, 0, 0],
+              [1, 0, 0, 0],
+              [1, 0, 0, 0],
+              [1, 0, 0, 0],  # 11
+              [0, 1, 0, 0],
+              [0, 0, 1, 0],
+              [0, 1, 0, 0],
+              [0, 1, 0, 0],
+              [0, 1, 0, 0],
+              [0, 1, 0, 0],
+              [0, 0, 0, 1],  # 18
+              [0, 0, 1, 0],
+              [0, 0, 1, 0],
+              [0, 0, 1, 0],
+              [0, 0, 1, 0],  # 22
+              [0, 0, 1, 0],
+              [0, 0, 0, 1],
+              [1, 0, 0, 0],
+              [0, 0, 0, 1],  # 26
+              [0, 0, 1, 0],
+              [0, 1, 0, 0],
+              [0, 0, 0, 1],
+              [0, 0, 0, 1],
+              [0, 1, 0, 0],
+              [0, 0, 0, 1],
+              [0, 0, 0, 1],
+              [1, 0, 0, 0],
+              [0, 1, 0, 0],  # 35
+              [0, 0, 0, 1],
+              [0, 0, 1, 0],
+              [0, 0, 0, 1],
+              [0, 1, 0, 0]]  # 39
 
     file_handler = FileHandler()
-    file_handler.update_path(folder="Literature")
+    file_handler.update_path(folder="Test")
     model = gensim.models.Doc2Vec.load(file_handler.get_models_folder() + "\\exampleEPOCH600.d2v")
-    document = file_handler.read_preprocessed("William Shakespeare.txt")
-    new_document = model.infer_vector(document)
-    print(model.docvecs.most_similar([new_document]))
-    #print(model.docvecs.most_similar(positive=document))
+    for i in range(len(model.docvecs)):
+        docs.append(model.docvecs[i])
 
+    x = np.array(docs, ndmin=2)
+    y = np.array(labels, ndmin=2)
+    classifier = TopicClassifier()
+    classifier.train(np.array(docs, ndmin=2), np.array(labels, ndmin=2))
 
+    document = file_handler.read_preprocessed("Networking.txt")
+    new_document = np.array(model.infer_vector(document), ndmin=2)
+    ret = classifier.predict(new_document)
+    print(ret)
+    """ 
+    
+    """
 
 """
     # Building the Doc2Vec model
---
+++ Research Stuff/Doc2Vec/TopicClassifier.py
@@ -0,0 +1,29 @@
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+
+
+class TopicClassifier:
+    """
+    This class is used for the classification of documents to the topics.
+    """
+
+    def __init__(self):
+        self.clf = MLPClassifier(activation='relu', learning_rate="constant", learning_rate_init=0.001)
+
+    def train(self, x: np.array, y: np.array) -> None:
+        """
+        Train the neural network classifier
+        :param x: input values
+        :param y: predict labels
+        """
+        self.clf.fit(x, y)
+
+    def predict(self, x: np.array) -> np.array:
+        """
+        Predict for given parameters
+        :param x: A document vector
+        :return: The topic predicted
+        """
+        val = self.clf.predict(x)
+
+        return val
